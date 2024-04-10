df4a7be183f65da7b21bcba44bde7bb44e3cadbf
OonoYouji <k023g0007@g.neec.ac.jp>
2024-04-10 10:04:47 +0900

    四則演算operatorを追加

--- DirectXGame/math/Vector2.h
+++ DirectXGame/math/Vector2.h
@@ -6,4 +6,101 @@
 struct Vector2 final {
 	float x;
 	float y;
+
+
+	Vector2 operator+ (const Vector2& other) const;
+	Vector2 operator- (const Vector2& other) const;
+	Vector2 operator/ (const Vector2& other) const;
+	Vector2 operator* (const Vector2& other) const;
+
+	Vector2 operator/ (float value) const;
+	Vector2 operator* (float value) const;
+
+
+	Vector2& operator+= (const Vector2& other);
+	Vector2& operator-= (const Vector2& other);
+	Vector2& operator/= (const Vector2& other);
+	Vector2& operator*= (const Vector2& other);
+
+	Vector2& operator/= (float value);
+	Vector2& operator*= (float value);
+
 };
+
+
+
+inline Vector2 Vector2::operator+(const Vector2& other) const {
+	return Vector2(
+		this->x + other.x,
+		this->y + other.y
+	);
+}
+inline Vector2 Vector2::operator-(const Vector2& other) const {
+	return Vector2(
+		this->x - other.x,
+		this->y - other.y
+	);
+}
+inline Vector2 Vector2::operator/(const Vector2& other) const {
+	return Vector2(
+		this->x / other.x,
+		this->y / other.y
+	);
+}
+inline Vector2 Vector2::operator*(const Vector2& other) const {
+	return Vector2(
+		this->x * other.x,
+		this->y * other.y
+	);
+}
+
+inline Vector2 Vector2::operator/(float value) const {
+	return Vector2(
+		this->x / value,
+		this->y / value
+	);
+}
+inline Vector2 Vector2::operator*(float value) const {
+	return Vector2(
+		this->x * value,
+		this->y * value
+	);
+}
+
+
+inline Vector2& Vector2::operator+=(const Vector2& other) {
+	this->x += other.x;
+	this->y += other.y;
+	return *this;
+}
+inline Vector2& Vector2::operator-=(const Vector2& other) {
+	this->x -= other.x;
+	this->y -= other.y;
+	return *this;
+}
+inline Vector2& Vector2::operator/=(const Vector2& other) {
+	this->x /= other.x;
+	this->y /= other.y;
+	return *this;
+}
+inline Vector2& Vector2::operator*=(const Vector2& other) {
+	this->x *= other.x;
+	this->y *= other.y;
+	return *this;
+}
+
+inline Vector2& Vector2::operator/=(float value) {
+	this->x /= value;
+	this->y /= value;
+	return *this;
+}
+inline Vector2& Vector2::operator*=(float value) {
+	this->x *= value;
+	this->y *= value;
+	return *this;
+}
+
+
+
+///- 名前を追加
+using Vec2f = Vector2;
--- DirectXGame/math/Vector3.h
+++ DirectXGame/math/Vector3.h
@@ -7,4 +7,110 @@ struct Vector3 final {
 	float x;
 	float y;
 	float z;
+
+
+	Vector3 operator+ (const Vector3& other) const;
+	Vector3 operator- (const Vector3& other) const;
+	Vector3 operator/ (const Vector3& other) const;
+	Vector3 operator* (const Vector3& other) const;
+
+	Vector3 operator/ (float value) const;
+	Vector3 operator* (float value) const;
+
+
+	Vector3& operator+= (const Vector3& other);
+	Vector3& operator-= (const Vector3& other);
+	Vector3& operator/= (const Vector3& other);
+	Vector3& operator*= (const Vector3& other);
+
+	Vector3& operator/= (float value);
+	Vector3& operator*= (float value);
+
 };
+
+
+
+
+
+inline Vector3 Vector3::operator+(const Vector3& other) const {
+	return Vector3(
+		this->x + other.x,
+		this->y + other.y,
+		this->z + other.z
+	);
+}
+inline Vector3 Vector3::operator-(const Vector3& other) const {
+	return Vector3(
+		this->x - other.x,
+		this->y - other.y,
+		this->z - other.z
+	);
+}
+inline Vector3 Vector3::operator/(const Vector3& other) const {
+	return Vector3(
+		this->x / other.x,
+		this->y / other.y,
+		this->z / other.z
+	);
+}
+inline Vector3 Vector3::operator*(const Vector3& other) const {
+	return Vector3(
+		this->x * other.x,
+		this->y * other.y,
+		this->z * other.z
+	);
+}
+
+inline Vector3 Vector3::operator/(float value) const {
+	return Vector3(
+		this->x / value,
+		this->y / value,
+		this->z / value
+	);
+}
+inline Vector3 Vector3::operator*(float value) const {
+	return Vector3(
+		this->x * value,
+		this->y * value,
+		this->z * value
+	);
+}
+
+
+inline Vector3& Vector3::operator+=(const Vector3& other) {
+	this->x += other.x;
+	this->y += other.y;
+	this->z += other.z;
+	return *this;
+}
+inline Vector3& Vector3::operator-=(const Vector3& other) {
+	this->x -= other.x;
+	this->y -= other.y;
+	this->z -= other.z;
+	return *this;
+}
+inline Vector3& Vector3::operator/=(const Vector3& other) {
+	this->x /= other.x;
+	this->y /= other.y;
+	this->z /= other.z;
+	return *this;
+}
+inline Vector3& Vector3::operator*=(const Vector3& other) {
+	this->x *= other.x;
+	this->y *= other.y;
+	this->z *= other.z;
+	return *this;
+}
+
+inline Vector3& Vector3::operator/=(float value) {
+	this->x /= value;
+	this->y /= value;
+	this->z /= value;
+	return *this;
+}
+inline Vector3& Vector3::operator*=(float value) {
+	this->x *= value;
+	this->y *= value;
+	this->z *= value;
+	return *this;
+}
