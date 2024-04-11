a850adb061c173b24dfb84e919ba9c06af63f752
OonoYouji <k023g0007@g.neec.ac.jp>
2024-04-11 11:44:25 +0900

    03_07_

--- DirectXGame/entity/Enemy.cpp
+++ DirectXGame/entity/Enemy.cpp
@@ -19,12 +19,24 @@ void Enemy::Init(Model* model, const Vec3f& position, uint32_t textureHandle) {
 
 	speed_ = 0.5f;
 	velocity_ = { 0.0f,0.0f,-speed_ };
+	approachVelocity_ = { 0.0f,0.0f,-1.0f };
+	leaveVelocity_ = { 0.0f,0.0f,0.5f };
 
+	phase_ = Phase::Approach;
 }
 
 void Enemy::Update() {
 
-	worldTransform_.translation_ += velocity_;
+	//worldTransform_.translation_ += velocity_;
+
+	switch(phase_) {
+	case Phase::Approach: ///- 接近
+		UpdateApproach();
+		break;
+	case Phase::Leave: ///- 離脱
+		UpdateLeave();
+		break;
+	}
 
 	///- 行列の更新
 	worldTransform_.UpdateMatrix();
@@ -35,4 +47,28 @@ void Enemy::Draw(const ViewProjection& viewProjection) {
 	///- 描画
 	model_->Draw(worldTransform_, viewProjection, textureHandle_);
 
+}
+
+
+
+/// -----------------------
+/// 接近時の更新処理
+/// -----------------------
+void Enemy::UpdateApproach() {
+	///- 移動
+	worldTransform_.translation_ += approachVelocity_;
+	///- 基底値になったら離脱
+	if(worldTransform_.translation_.z < 0.0f) {
+		phase_ = Phase::Leave;
+	}
+}
+
+
+
+/// -----------------------
+/// 離脱時の更新処理
+/// -----------------------
+void Enemy::UpdateLeave() {
+	///- 移動
+	worldTransform_.translation_ += leaveVelocity_;
 }
--- DirectXGame/entity/Enemy.h
+++ DirectXGame/entity/Enemy.h
@@ -7,6 +7,16 @@
 
 
 
+///- Enemyの状態
+enum class Phase : int32_t {
+	Approach,	//- 接近
+	Leave,		//- 離脱
+};
+
+
+/// -------------------------
+/// 敵
+/// -------------------------
 class Enemy {
 public:
 
@@ -19,12 +29,22 @@ public:
 
 private: ///- メンバ変数
 
+	///- モデルと画像
 	std::unique_ptr<Model> model_;
 	uint32_t textureHandle_;
 
+	///- world座標
 	WorldTransform worldTransform_;
 
 	Vec3f velocity_;
+	Vec3f approachVelocity_;
+	Vec3f leaveVelocity_;
 	float speed_;
+
+	///- 状態
+	Phase phase_;
+
+	void UpdateApproach();
+	void UpdateLeave();
 
 };
