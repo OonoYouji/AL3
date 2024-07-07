f2d4bbe2155c1a524dfcc7f9ca5a0dc5763c5f4a
OonoYouji <k023g0007@g.neec.ac.jp>
2024-07-07 23:42:54 +0900

    GameObjectクラスにSetterを用意

--- DirectXGame/Game/3d/GameObject/GameObject.cpp
+++ DirectXGame/Game/3d/GameObject/GameObject.cpp
@@ -1,18 +1,19 @@
 #include "GameObject.h"
 
 #include <Mat4Math.h>
+#include <GameObjectManager.h>
 
+GameObject::GameObject() {
 
-GameObjcet::GameObjcet() {}
-GameObjcet::~GameObjcet() {}
+}
 
-void GameObjcet::UpdateMatrix() {
+void GameObject::UpdateMatrix() {
 	worldTransform_.matWorld_ = MakeAffine(worldTransform_.scale_, worldTransform_.rotation_, worldTransform_.translation_);
 	if(parent_) {
 		worldTransform_.matWorld_ *= parent_->GetMatTransform();
 	}
 }
 
-const Mat4& GameObjcet::GetMatTransform() const {
+const Mat4& GameObject::GetMatTransform() const {
 	return worldTransform_.matWorld_;
 }
--- DirectXGame/Game/3d/GameObject/GameObject.h
+++ DirectXGame/Game/3d/GameObject/GameObject.h
@@ -5,11 +5,11 @@
 /// <summary>
 /// すべてのゲームオブジェクトの基本となるクラス
 /// </summary>
-class GameObjcet {
+class GameObject {
 public:
 
-	GameObjcet();
-	~GameObjcet();
+	GameObject();
+	virtual ~GameObject() = default;
 
 public:
 
@@ -23,10 +23,28 @@ public:
 
 	const Mat4& GetMatTransform() const;
 
+#pragma region Setter
+	void SetPosX(float x) { worldTransform_.translation_.x = x; }
+	void SetPosY(float y) { worldTransform_.translation_.y = y; }
+	void SetPosZ(float z) { worldTransform_.translation_.z = z; }
+	void SetPos(const Vec3f& v) { worldTransform_.translation_ = v; }
+
+	void SetRotateX(float x) { worldTransform_.rotation_.x = x; }
+	void SetRotateY(float y) { worldTransform_.rotation_.y = y; }
+	void SetRotateZ(float z) { worldTransform_.rotation_.z = z; }
+	void SetRotate(const Vec3f& v) { worldTransform_.rotation_ = v; }
+
+	void SetScaleX(float x) { worldTransform_.scale_.x = x; }
+	void SetScaleY(float y) { worldTransform_.scale_.y = y; }
+	void SetScaleZ(float z) { worldTransform_.scale_.z = z; }
+	void SetScale(const Vec3f& v) { worldTransform_.scale_ = v; }
+#pragma endregion
+
+
 private:
 
 	WorldTransform worldTransform_;
 
-	GameObjcet* parent_;
+	GameObject* parent_;
 
 };
