289e50747b830df4d6185d876024eae49d367256
OonoYouji <k023g0007@g.neec.ac.jp>
2024-07-08 14:03:12 +0900

    Sceneの基底クラスを作成

--- DirectXGame/Game/Scene/BaseScene.cpp
+++ DirectXGame/Game/Scene/BaseScene.cpp
@@ -1,1 +1,49 @@
 #include "BaseScene.h"
+
+#include "DirectXCommon.h"
+#include "Sprite.h"
+#include "Model.h"
+
+namespace {
+	DirectXCommon* dxCommon;
+}
+
+BaseScene::BaseScene() {
+	dxCommon = DirectXCommon::GetInstance();
+	input_ = Input::GetInstance();
+	audio_ = Audio::GetInstance();
+}
+
+void BaseScene::Draw() {
+	
+	// コマンドリストの取得
+	ID3D12GraphicsCommandList* commandList = dxCommon->GetCommandList();
+
+
+	/// ---------------------------------------------------
+	/// 背景Spriteの描画
+	/// ---------------------------------------------------
+	Sprite::PreDraw(commandList);
+	BackSpriteDraw();
+	Sprite::PostDraw();
+	dxCommon->ClearDepthBuffer();
+	
+	
+	/// ---------------------------------------------------
+	/// 3dObjectの描画
+	/// ---------------------------------------------------
+	Model::PreDraw(commandList);
+	Object3dDraw();
+	Model::PostDraw();
+	
+
+
+	/// ---------------------------------------------------
+	/// 前景Spriteの描画
+	/// ---------------------------------------------------
+	Sprite::PreDraw(commandList);
+	FrontSpriteDraw();
+	Sprite::PostDraw();
+	
+
+}
--- DirectXGame/Game/Scene/BaseScene.h
+++ DirectXGame/Game/Scene/BaseScene.h
@@ -1,6 +1,40 @@
 #pragma once
 
+#include "Audio.h"
+#include "Input.h"
+
+
+/// <summary>
+/// すべてのSceneの基底クラス
+/// </summary>
 class BaseScene {
 public:
+
+	BaseScene();
+	virtual ~BaseScene() = default;
+
+	virtual void Initialize() = 0;
+	virtual void Update() = 0;
+	void Draw();
+
+	/// <summary>
+	/// 背景Spriteの描画
+	/// </summary>
+	virtual void BackSpriteDraw() = 0;
+
+	/// <summary>
+	/// 3dObjectの描画
+	/// </summary>
+	virtual void Object3dDraw() = 0;
+
+	/// <summary>
+	/// 前景Spriteの描画
+	/// </summary>
+	virtual void FrontSpriteDraw() = 0;
+
+protected:
+
+	Input* input_ = nullptr;
+	Audio* audio_ = nullptr;
 
 };
